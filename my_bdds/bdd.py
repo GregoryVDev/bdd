4fd2ea254d45b5cf179f0a2fc4044f37c3f985ea
GregoryVDev <arexo@hotmail.fr>
2025-01-30 16:56:47 +0100

    Connected to the database

--- my_bdds/bdd.py
+++ my_bdds/bdd.py
@@ -5,3 +5,5 @@ import sqlite3
 # Créer une variable pour mettre le chemin en spécifiant la base de donnée avec l'extension
 bdd = "my_bdds/ma_base-de_donnees.sq3"
 
+# On se connecte à la base de données avec la variable "bdd"
+connexion = sqlite3.connect(bdd)
