bbd042f3e1695474f4d73fddd17c104ee0edab70
GregoryVDev <arexo@hotmail.fr>
2025-01-30 16:55:12 +0100

    Imported the sqlite3 database and created a variable for the path by specifying the database with the extension

---
+++ my_bdds/bdd.py
@@ -0,0 +1,7 @@
+# Création de la base de donnée
+
+import sqlite3
+
+# Créer une variable pour mettre le chemin en spécifiant la base de donnée avec l'extension
+bdd = "my_bdds/ma_base-de_donnees.sq3"
+
